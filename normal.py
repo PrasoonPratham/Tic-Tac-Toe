35a22ceb7ebd78b92fa3f9836c783d2beb95ce51
Pratham Prasoon <42303128+PrasoonPratham@users.noreply.github.com>
2021-09-08 13:45:06 +0530

    working non-computer game

--- normal.py
+++ normal.py
@@ -1,3 +1,5 @@
+import sys
+
 # Board
 board = {7: ' ', 8: ' ', 9: ' ',
          4: ' ', 5: ' ', 6: ' ',
@@ -7,15 +9,86 @@ board = {7: ' ', 8: ' ', 9: ' ',
 turn = 'X'
 
 # Count
-c = 0
+count = 0
 
 # Draw the board
 def draw():
     print(board[7], "|" ,board[8], "|" ,board[9])
-    print("---------")
+    print("--+---+--")
     print(board[4], "|" ,board[5], "|" ,board[6])
-    print("---------")
+    print("--+---+--")
     print(board[1], "|" ,board[2], "|" ,board[3])
 
+# Main loop
 def game():
-    input("")
+
+    # Global variables
+    global count, turn
+
+    # Checking to see if the player has won
+    def checkwin():
+        global count, turn
+        if board[1] == board[2] == board[3] == turn:
+            print("Player ", turn, " wins!")
+            sys.exit()
+        elif board[4] == board[5] == board[6] == turn:
+            print("Player ", turn, " wins!")
+            sys.exit()
+        elif board[7] == board[8] == board[9] == turn:
+            print("Player ", turn, " wins!")
+            sys.exit()
+        elif board[1] == board[4] == board[7] == turn:
+            print("Player ", turn, " wins!")
+            sys.exit()
+        elif board[2] == board[5] == board[8] == turn:
+            print("Player ", turn, " wins!")
+            sys.exit()
+        elif board[3] == board[6] == board[9] == turn:
+            print("Player ", turn, " wins!")
+            sys.exit()
+        elif board[1] == board[5] == board[9] == turn:
+            print("Player ", turn, " wins!")
+            sys.exit()
+        elif board[3] == board[5] == board[7] == turn:
+            print("Player ", turn, " wins!")
+            sys.exit()
+            
+    # Play until the game is over
+    while count<10:
+        checkwin()
+        if count % 2 == 0:
+            ans = int(input("Player X, which place would you like to place your move? "))
+            if ans < 10 and ans > 0:
+                if board[ans] == ' ':
+                    board[ans] = 'X'
+                    count += 1
+                    draw()
+                else:
+                    print("That place is already taken!")
+                    game()
+            else:
+                print("Please enter a number between 1 and 9!")
+                game()
+
+        elif count % 2 == 1:
+            ans = int(input("Player O, which place would you like to place your move? "))
+            if ans < 10 and ans > 0:
+                if board[ans] == ' ':
+                    board[ans] = 'O'
+                    count += 1
+                    draw()
+                else:
+                    print("That place is already taken!")
+                    game()
+            else:
+                print("Please enter a number between 1 and 9!")
+                game()
+
+    #Tie
+    else:
+        print("It's a tie!")
+    
+game()
+
+
+
